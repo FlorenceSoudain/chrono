78394349ff0d2d2d35cf5f9e3f0a193e47a02a8c
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2018-11-20 15:51:18 +0100

    V2

--- script.js
+++ script.js
@@ -4,37 +4,42 @@ var heures = document.getElementById('heures');
 var time;
 document.getElementById('stop').style.display = "none";
 var NbrSecondes = 60;
-var NbrMinutes = 60;
-var NbrHeures = 24;
-function FSecondes()
+var NbrMinutes = 59;
+var NbrHeures = 23;
+function chrono()
 {
-    time = setTimeout(FSecondes, 1000);
-    NbrSecondes += -1;
+    time = setTimeout(chrono, 1000);
+
+    if(NbrSecondes < 1)
+    {
+        NbrSecondes = 60;
+        NbrMinutes--;
+    }
+    if (NbrMinutes < 0)
+    {
+        NbrMinutes = 60;
+        NbrHeures--;
+    }
+
+
+    NbrSecondes--;
     secondes.innerHTML = NbrSecondes + " secondes ";
     minutes.innerHTML = NbrMinutes + " minutes";
     heures.innerHTML = NbrHeures + " heures";
-    if(NbrSecondes == 0)
-    {
-        NbrSecondes = 60;
-        NbrMinutes += -1;
-    }
-    if (NbrMinutes == 0)
-    {
-        NbrMinutes = 60;
-        NbrHeures += -1;
 
+    if (NbrHeures == 0 && NbrMinutes == 0 && NbrSecondes == 0)
+    {
+        clearTimeout(time);
+        document.getElementById('stop').style.visibility = "hidden";
     }
-
 }
 
 function stop() {
     clearTimeout(time);
 }
 
-
-
 document.getElementById('start').addEventListener('click', function () {
-    FSecondes();
+    chrono();
     document.getElementById('start').style.display ="none";
     document.getElementById('stop').style.display = "block";
 });
@@ -43,4 +48,46 @@ document.getElementById('stop').addEventListener('click', function () {
     stop();
     document.getElementById('start').style.display ="block";
     document.getElementById('stop').style.display = "none";
+});
+
+document.getElementById('envoyer').addEventListener('click', function () {
+
+    var valeurSecondes = document.getElementById('champSecondes').value;
+    var valeurMinutes = document.getElementById('champMinutes').value;
+    var valeurHeures = document.getElementById('champHeures').value;
+
+    if  (valeurSecondes)
+    {
+        NbrSecondes = valeurSecondes;
+    }
+    else
+    {
+        NbrSecondes = 0;
+    }
+    if (valeurMinutes)
+    {
+        NbrMinutes = valeurMinutes;
+    }
+    else
+    {
+        NbrMinutes = 0;
+    }
+    if (valeurHeures)
+    {
+        NbrHeures = valeurHeures;
+    }
+    else
+    {
+        NbrHeures = 0;
+    }
+
+    document.getElementById('start').style.display ="none";
+    document.getElementById('stop').style.display = "block";
+    document.getElementById('envoyer').style.display ="none";
+
+    chrono();
+});
+
+document.getElementById('retour').addEventListener('click', function () {
+    window.location.reload();
 });
